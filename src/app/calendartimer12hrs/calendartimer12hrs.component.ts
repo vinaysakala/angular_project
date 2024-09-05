b9d724d57fd4ece93f3eecf6e30260e1689a3337
vinaysakala <vinaysakala2002@gmail.com>
2024-09-05 18:41:31 +0530

    05/09/2024

--- src/app/calendartimer12hrs/calendartimer12hrs.component.ts
+++ src/app/calendartimer12hrs/calendartimer12hrs.component.ts
@@ -6,26 +6,32 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./calendartimer12hrs.component.css']
 })
 export class Calendartimer12hrsComponent {
-  private _datepipe=inject(DatePipe);
+  private _datepipe = inject(DatePipe);
   public calModel: any;
-  public selectedDate:any;
+  public selectedDate: any;
+  public texxt:any=null;
 
   onsave() {
     let now = new Date();
-    let currenthours=now.getHours();
-    let currentmin=now.getMinutes();
-    let selectedtime=this._datepipe.transform(new Date(this.calModel),'HH:mm')
-    let selecthours=selectedtime?.split(':')[0];
-    let selectmin=selectedtime?.split(':')[1];
-    if(selecthours!=null && selectmin!=null ){
-      if(parseInt(selecthours) < currenthours){
-         console.log("time should be greater then current time") 
-      }  else if(parseInt(selectmin) < currentmin){
-        console.log("time should be greater then current time")
+    let currenthours = now.getHours();
+    let currentmin = now.getMinutes();
+    let selectedtime = this._datepipe.transform(new Date(this.calModel), 'HH:mm')
+    let selecthours = selectedtime?.split(':')[0];
+    let selectmin = selectedtime?.split(':')[1];
+    if (selecthours != null && selectmin != null) {
+      if (parseInt(selecthours) < currenthours) {
+      this.texxt='time should be greater then current time';
+      } else if (parseInt(selecthours) == currenthours) {
+        if (parseInt(selectmin) < currentmin) {
+         this.texxt='time should be greater then current time';
+        }else{
+          this.texxt='correct time';
+        }
+      }else{
+        this.texxt='correct time';
+      }
+    }else{
+      this.texxt='please select time';
     }
-    }
-   
-
-
   }
 }
